6ff111397a778c085550bad105ddf05766e98a12
briecodes <hello@brie.work>
2018-06-19 19:10:30 -0400

    test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div>
+        test
         <button name="all_productions">All Productions</button>
         <button name="create_new_production">Create New Production</button>
         <button name="my_productions">My Productions</button>
