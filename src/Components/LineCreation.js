ffcbc415f1adc86a3e451be15b892704e8d5db74
William Dale <williambdale@gmail.com>
2018-06-19 11:25:08 -0400

    Add LineInputForm to LineCreation

--- src/Components/LineCreation.js
+++ src/Components/LineCreation.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import LineInputForm from './LineInputForm'
+
 class LineCreation extends Component {
   render() {
     return (
-
+      <LineInputForm />
     )
   }
 }
