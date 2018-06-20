5e55f9200635ea32ca152fbdbd1e3a0109d04414
briecodes <hello@brie.work>
2018-06-19 21:43:35 -0400

    commented lines deleted.

--- src/Containers/ActorsDisplay.js
+++ src/Containers/ActorsDisplay.js
@@ -2,27 +2,10 @@ import React, { Component } from 'react';
 
 import ActorCard from '../Components/ActorCard'
 
-// const URL = 'https://mod-4-backend.herokuapp.com/api/v1/'
-
 class AllActorsDisplay extends Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   actors: []
-    // }
   }
-
-  // componentDidMount(){
-  //   this.fetchActorData();
-  // }
-
-  // fetchActorData = () => {
-  //   fetch(URL + 'actors').then(response => response.json()).then(actors => {
-  //     this.setState({
-  //       actors
-  //     });
-  //   });
-  // };
 
   renderActorCards = () => {
     const cards = this.props.actors.map((actor, index) => {
