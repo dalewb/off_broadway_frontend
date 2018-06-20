10e2fa3d2bfc52594b78365a4b75dbd21ae7f890
briecodes <hello@brie.work>
2018-06-19 23:08:13 -0400

    merge conflict

--- src/Components/ActorCard.js
+++ src/Components/ActorCard.js
@@ -11,11 +11,7 @@ class ActorCard extends Component {
 
   renderActor = () => {
     return (
-<<<<<<< HEAD
       <div onClick={this.props.handleClick} className='actor_card' >
-=======
-      <div onClick={this.handleActorClick} className='actor_card' >
->>>>>>> brie
         <img src={this.props.actor.img_url} alt='' />
         <p>Name: {this.props.actor.name}</p>
       </div>
