50b28188f8ae3e782d81ea8b169160587098fb8c
William Dale <williambdale@gmail.com>
2018-06-19 22:23:45 -0400

    add handleClick on actorCard

--- src/Components/ActorCard.js
+++ src/Components/ActorCard.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 
 class ActorCard extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
 
-  handleActorClick = () => {
+  }
+
+  handleActorClick() {
     this.props.handleClick(this.props.actor)
   }
 
   renderActor = () => {
     let actor = this.props.actor
     return (
-<<<<<<< HEAD
       <div onClick={this.handleActorClick}>
-=======
-      <div onClick={() => this.props.handleClick(this.props.actor)} className='actor_card' >
-        <img src={this.props.actor.img_url} alt='' />
->>>>>>> brie
         <p>Name: {this.props.actor.name}</p>
+        <img src={this.props.actor.img_url} />
       </div>
     )
   }
