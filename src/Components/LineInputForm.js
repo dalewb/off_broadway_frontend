bf716fb4b1374737d61667a6c12b98e5ada8e80b
briecodes <hello@brie.work>
2018-06-20 21:31:27 -0400

    const options added. edited to include select character.

--- src/Components/LineInputForm.js
+++ src/Components/LineInputForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Form, Input, Select } from 'semantic-ui-react';
 
 class LineInput extends Component {
 
   render() {
+    const options = [
+      { key: 1, text: "Select Character", value: "" },
+      { key: 2, text: "Character A", value: "A" },
+      { key: 3, text: "Character B", value: "B" },
+    ]
     return (
       <React.Fragment>
         <select onChange={this.props.handleSelect} name={this.props.char}>
