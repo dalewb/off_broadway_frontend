c4d5109fa8ca8ae83cc49b9aa764c579832b9efb
briecodes <hello@brie.work>
2018-06-20 21:43:38 -0400

    Form brought over with removal of labels.

--- src/Components/LineInputForm.js
+++ src/Components/LineInputForm.js
@@ -5,19 +5,29 @@ class LineInput extends Component {
 
   render() {
     const options = [
-      { key: 1, text: "Select Character", value: "" },
-      { key: 2, text: "Character A", value: "A" },
-      { key: 3, text: "Character B", value: "B" },
+      { key: 1, text: "Character A", value: "A" },
+      { key: 2, text: "Character B", value: "B" }
     ]
     return (
-      <React.Fragment>
-        <select onChange={this.props.handleSelect} name={this.props.char}>
-          <option value="" >Select Character</option>
-          <option value="A" >Character A</option>
-          <option value="B" >Character B</option>
-        </select>
-        <textarea name={this.props.name} onChange={this.props.handleChange} />
-      </React.Fragment>
+      <Form.Group centered >
+        <Form.Field
+          control={Select}
+          // label="Select Character"
+          placeholder="Select Character"
+          options={options}
+          compact
+          onChange={this.props.handleSelect}
+          name={this.props.char}
+        />
+        <Form.Field
+          control={Input}
+          // label="Enter Line"
+          placeholder="Enter Line"
+          name={this.props.name}
+          onChange={this.props.handleChange}
+          style={{ width: "500px" }}
+        />
+      </Form.Group>
     )
   }
 }
