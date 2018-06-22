5d01ec99e5473be941b8c64d101731df0b8107a2
briecodes <hello@brie.work>
2018-06-22 01:08:36 -0400

    working manual dialogue exchange

--- src/Components/AR.js
+++ src/Components/AR.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 
 class ARpage extends Component {
+  state = {
+    currentLine: 1
+  }
+
+  componentDidMount() {
+    this.openCurtains();
+  }
+
+  dialogue = '';
+
+  renderDialogue = () => {
+    switch (this.state.currentLine) {
+      case 1:
+        this.dialogue = <div className={`${this.props.show.script.line_1[0]}-bubble`} id='line_1' >{this.props.show.script.line_1}</div>
+        break;
+      case 2:
+        this.dialogue = <div className={`${this.props.show.script.line_2[0]}-bubble`} id='line_2' >{this.props.show.script.line_2}</div>
+        break;
+      case 3:
+        this.dialogue = <div className={`${this.props.show.script.line_3[0]}-bubble`} id='line_3' >{this.props.show.script.line_3}</div>
+        break;
+      case 4:
+        this.dialogue = <div className={`${this.props.show.script.line_4[0]}-bubble`} id='line_4' >{this.props.show.script.line_4}</div>
+        break;
+      case 5:
+        this.dialogue = <div className={`${this.props.show.script.line_5[0]}-bubble`} id='line_5' >{this.props.show.script.line_5}</div>
+        break;
+      default:
+        this.dialogue = <div></div>
+    };
+  };
+
+  openCurtains = () => {
+    // const leftCurtain = document.getElementById('img_l-curtain');
+    // const rightCurtain = document.getElementById('img_r-curtain');
+    // leftCurtain.style.left = `0px`;
+    // rightCurtain.style.right = `0px`;
+    // let i = 0;
+    // while (i < 50){
+    //   leftCurtain.style.left = `${i * (-1)}px`
+    //   rightCurtain.style.right = `${i * (-1)}px`
+    //     i++;
+    // };
+  };
+
+  cycleDialogue = () => {
+    if(this.state.currentLine < 5){
+      this.setState({
+        currentLine: ++this.state.currentLine
+      });
+    }else{
+      this.setState({
+        currentLine: 1
+      });
+    };
+  };
+
   render() {
-    console.log('inside AR file, svg:', this.props.show.actors[0].svg_url);
+    this.renderDialogue();
     return (
       <React.Fragment>
-        <div id='showDiv'>
+        <div id='showDiv' onClick={this.cycleDialogue} >
+          {/* <div className={`${this.props.show.script.line_1[0]}-bubble`} id='line_1' >{this.props.show.script.line_1}</div> */}
+          {this.dialogue}
           <img id='img_chairs' src='/assets/stage_setup_chairs.png' alt='chairs' />
           <img id='img_l-curtain' src='/assets/stage_setup_l-curtain.png' alt='left curtain' />
           <img id='img_r-curtain' src='/assets/stage_setup_r-curtain.png' alt='right curtain' />
