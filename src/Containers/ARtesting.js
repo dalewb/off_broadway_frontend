924d5cb105c0510be3d81498da365402c3df82ec
briecodes <hello@brie.work>
2018-06-21 20:37:13 -0400

    three.js working!!

--- src/Containers/ARtesting.js
+++ src/Containers/ARtesting.js
@@ -9,34 +9,69 @@ let THREE = require('three');
 
 // let MyLoader = require('../assets/JDLoader.min.js');
 
+//   let scene = new THREE.Scene();
+//   let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+//   let renderer = new THREE.WebGLRenderer();
+//   renderer.setSize( window.innerWidth, window.innerHeight );
+//    let div = document.getElementById('canvasholder');
+//   document.body.appendChild( renderer.domElement );
+
+//   let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//   let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//   let cube = new THREE.Mesh( geometry, material );
+//   scene.add( cube );
+
+//   camera.position.z = 5;
+
+//   let animate = function () {
+//     requestAnimationFrame( animate );
+
+//     cube.rotation.x += 0.1;
+//     cube.rotation.y += 0.1;
+
+//     renderer.render( scene, camera );
+//   };
+
+//   animate();
 
 class ARtesting extends Component {
-  // let scene = new THREE.Scene();
-  // let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-  // let renderer = new THREE.WebGLRenderer();
-  // renderer.setSize( window.innerWidth, window.innerHeight );
-  // document.body.appendChild( renderer.domElement );
+    getItStarted = () => {
+        let scene = new THREE.Scene();
+        let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight/2, 0.1, 1000);
 
-  // let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  // let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  // let cube = new THREE.Mesh( geometry, material );
-  // scene.add( cube );
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        let divvy = document.getElementById('canvasholder');
+        divvy.appendChild(renderer.domElement);
 
-  // camera.position.z = 5;
+        let geometry = new THREE.BoxGeometry(1, 1, 1);
+        let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        let cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
 
-  // let animate = function () {
-  //   requestAnimationFrame( animate );
+        camera.position.z = 5;
 
-  //   cube.rotation.x += 0.1;
-  //   cube.rotation.y += 0.1;
+        let animate = function () {
+            requestAnimationFrame(animate);
 
-  //   renderer.render( scene, camera );
-  // };
+            cube.rotation.x += 0.1;
+            cube.rotation.y += 0.1;
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+    }
+
+    componentDidMount() {
+        this.getItStarted();
+    }
+
 
   render() {
-    // this.animate();
-    alert(`H: ${window.innerHeight}. W: ${window.innerWidth}`);
+    // alert(`H: ${window.innerHeight}. W: ${window.innerWidth}`);
     return (
         <div id="canvasholder">
             <span className="title">Tap to spawn objects on surfaces.</span><br/>
