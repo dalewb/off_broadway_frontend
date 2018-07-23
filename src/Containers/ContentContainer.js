a5846a28882bf3398236a60173f79d31af3f4947
briecodes <hello@brie.work>
2018-07-23 14:46:22 -0400

    removed commented-out console log.

--- src/Containers/ContentContainer.js
+++ src/Containers/ContentContainer.js
@@ -81,8 +81,6 @@ class ContentContainer extends Component {
       myProductions: [...this.state.myProductions, viewProduction],
       productions: [...this.state.productions, viewProduction]
     });
-    // console.log("View Production", this.state.viewProduction);
-    // console.log("In View Production, myProductions", this.state.myProductions);
     this.props.onClickHandler('my productions')
   };
 
