081363562ad91bb681119ff5bab965bbbe760c08
briecodes <hello@brie.work>
2018-07-24 18:05:44 -0400

    removed serviceWorker.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
