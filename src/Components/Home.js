f929a8865032dda7103564d557e674762412ad36
briecodes <hello@brie.work>
2018-07-23 22:55:07 -0400

    refactored into functional component.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <span id='welcome-headline'>Welcome to OFF-BROADWAY!</span>
-        <span id='welcome-subhead'>The only place on the interwebs where you can write 5 lines of script and choose your own pre-determined actors to fill the roles!</span>
-        <object id='group-shot' aria-label='Actor Group' data='/assets/standup.svg' type="image/svg+xml"></object>
-      </React.Fragment>
-    )
-  }
-}
+const Home = () => (
+  <React.Fragment>
+    <span id='welcome-headline'>Welcome to OFF-BROADWAY!</span>
+    <span id='welcome-subhead'>The only place on the interwebs where you can write 5 lines of script and choose your own pre-determined actors to fill the roles!</span>
+    <object id='group-shot' aria-label='Actor Group' data='/assets/standup.svg' type="image/svg+xml"></object>
+  </React.Fragment>
+)
 
 export default Home;
