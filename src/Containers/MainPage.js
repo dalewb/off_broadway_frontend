c72e99e1802ecb47b8f169acc22c1a4adf9279dd
briecodes <hello@brie.work>
2018-07-23 23:10:11 -0400

    createContainer renamed. actorsDisplay moved to components.

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Header from '../Components/Header'
 import AllProductions from '../Components/AllProductions';
-import CreateContainer from './CreateContainer';
+import CreateContainer from '../Components/CreateNewProduction';
 import Home from '../Components/Home';
 import ARpage from '../Components/AR';
 
