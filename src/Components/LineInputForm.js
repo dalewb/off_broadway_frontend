481fc292405ddd3574b69a8f96283f5338a78cfb
briecodes <hello@brie.work>
2018-07-24 09:00:31 -0400

    refactored from class to functional component.

--- src/Components/LineInputForm.js
+++ src/Components/LineInputForm.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Input } from 'semantic-ui-react';
 
-class LineInput extends Component {
-
-  render() {
-    return (
-      <Form.Group >
-        <select defaultValue='Select Character' onChange={this.props.handleSelect} id={this.props.char}>
-          <option value='Select Character' disabled>Select Character</option>
-          <option value='A'>Character A</option>
-          <option value='B'>Character B</option>
-        </select>
-        <Form.Field
-          control={Input}
-          placeholder="Enter Line"
-          name={this.props.name}
-          onChange={this.props.handleChange}
-          style={{ width: "500px" }}
-          value={this.props.lineText}
-        />
-      </Form.Group>
-    )
-  }
-}
+const LineInput = (props) => (
+  <Form.Group >
+    <select defaultValue='Select Character' onChange={props.handleSelect} id={props.char}>
+      <option value='Select Character' disabled>Select Character</option>
+      <option value='A'>Character A</option>
+      <option value='B'>Character B</option>
+    </select>
+    <Form.Field
+      control={Input}
+      placeholder="Enter Line"
+      name={props.name}
+      onChange={props.handleChange}
+      style={{ width: "500px" }}
+      value={props.lineText}
+    />
+  </Form.Group>
+)
 
 export default LineInput;
