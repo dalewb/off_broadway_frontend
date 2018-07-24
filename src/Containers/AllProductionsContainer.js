a12379eec5ff23559ff9b458bd32cf84ca5d9bc1
briecodes <hello@brie.work>
2018-07-23 22:27:55 -0400

    allProductionsDisplay lifted to allProductionsContainer and marked for deletion.

--- src/Containers/AllProductionsContainer.js
+++ src/Containers/AllProductionsContainer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-import AllProductionsDisplay from './AllProductionsDisplay'
+// import AllProductionsDisplay from './AllProductionsDisplay'
+import ProductionCard from '../Components/ProductionCard';
+import ProductionReview from '../Components/ProductionReview';
 
 const URL = 'https://mod-4-backend.herokuapp.com/api/v1/'
 
 class AllProductionsContainer extends Component {
   state = {
-    productions: []
+    productions: [],
+    productionReview: null
   }
 
   componentDidMount() {
@@ -26,10 +29,33 @@ class AllProductionsContainer extends Component {
     });
   };
 
+  renderProductionCards = () => {
+    const cards = this.state.productions.map((production, index) => {
+      return <ProductionCard production={production} key={index} onClickHandler={this.setProductionReview} />
+    });
+    return cards;
+  };
+
+  setProductionReview = (id) => {
+    const productionReview = this.state.productions.find(prod => {
+      return prod.script_id === id
+    });
+    this.setState({
+      productionReview
+    });
+  };
+
   render() {
+    const pCards = this.renderProductionCards();
     return (
       <div id='productionDisplay'>
-        <AllProductionsDisplay allProductions={this.state.productions} handleViewProductionClick={this.props.handleViewProductionClick}/>
+        <div className="ui three cards centered">
+          {this.state.productionReview ? <ProductionReview production={this.state.productionReview} handleViewProductionClick={() => this.props.handleViewProductionClick(this.state.productionReview.id)} /> : null}
+        </div>
+        <div className="ui five cards centered">
+          {pCards}
+        </div>
+        {/* <AllProductionsDisplay allProductions={this.state.productions} handleViewProductionClick={this.props.handleViewProductionClick}/> */}
         <div className='divider'></div>
       </div>
     )
