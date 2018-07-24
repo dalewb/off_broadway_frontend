61c738df4b10729d1a7e4846b0bc1cf0628c5ad3
briecodes <hello@brie.work>
2018-07-23 22:52:08 -0400

    refactored into functional component.

--- src/Components/ProductionCard.js
+++ src/Components/ProductionCard.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 
-class ProductionCard extends Component {
-
-  renderProductionCard() {
-    return (
-      <Card onClick={() => this.props.onClickHandler(this.props.production.script_id)}>
-        <Image src={this.props.production.script.img_url} alt='' />
-        <div>
-          <span className='cardTitle'>{this.props.production.script.title}</span>
-          <span className='cardCopy'>Written by: {this.props.production.user.username}</span>
-        </div>
-      </Card>
-    )
-  }
-
-  render() {
-    return (
-      this.renderProductionCard()
-    )
-  }
-}
+const ProductionCard = (props) => (
+  <Card onClick={() => props.onClickHandler(props.production.script_id)}>
+    <Image src={props.production.script.img_url} alt='' />
+    <div>
+      <span className='cardTitle'>{props.production.script.title}</span>
+      <span className='cardCopy'>Written by: {props.production.user.username}</span>
+    </div>
+  </Card>
+);
 
 export default ProductionCard;
